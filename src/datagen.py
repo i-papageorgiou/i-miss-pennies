3ac52a5af99aa7293681e9278ec119356f828dd3
Natasha Guharoy <55326574+TashTheInventor@users.noreply.github.com>
2026-09-18 11:49:21 -0400

    fixed generation of decks and .count

--- src/datagen.py
+++ src/datagen.py
@@ -6,20 +6,31 @@ import random
 
 
 def generate_deck():
-    suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
-    deck = [{'suit': suit, 'rank': rank} for suit in suits for rank in ranks]
+    #suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+    #ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+    #deck = [{'suit': suit, 'rank': rank} for suit in suits for rank in ranks]
+    
+    #create list of 26 Red and 26 Black
+    red_list = [f"R" for i in range(1, 27)]
+    black_list = [f"B" for i in range(27, 53)]
+    deck = red_list + black_list
+
     return deck
 
 # shuffle
-
-for iter in range(1, 11):
+shuffled_decks = []
+for iter in range(1, 3):
     deck = generate_deck()
-    deck_items = list(deck.items())
-    random.shuffle(deck_items)
-    shuffled_deck = dict(deck_items)
-    # store the shuffled deck in a variable named Deck_{iter}
-    globals()[f'Deck_{iter}'] = shuffled_deck
+    random.shuffle(deck)
+    condensed_deck = "".join(deck)
 
+    shuffled_decks.append(condensed_deck)
+    
+#print(shuffled_decks[0][0:3])
+
+print(shuffled_decks)
+print(type(shuffled_decks[0]))
+deck_1 = shuffled_decks[0]
+print(deck_1.count('R'))
 # store the raw deck data
 # try to not use .csv
--- src/dataproc.py
+++ src/dataproc.py
@@ -1,5 +1,5 @@
 # Data Processing - scoring and summarizing 
-
+card_comb = ["RRR", "BBB", ]
 # ensure simulation is running unique instances every run
 
 # shuffle the deck on each iter
