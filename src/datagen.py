d9e994f4194375694229281a129a795132a86d63
Ilias Papageorgiou <thg9npb5ck@privaterelay.appleid.com>
2026-09-21 11:16:22 -0400

    Finalize datagen phase

--- src/datagen.py
+++ src/datagen.py
@@ -6,31 +6,27 @@ import random
 
 
 def generate_deck():
-    #suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-    #ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
-    #deck = [{'suit': suit, 'rank': rank} for suit in suits for rank in ranks]
-    
-    #create list of 26 Red and 26 Black
-    red_list = [f"R" for i in range(1, 27)]
-    black_list = [f"B" for i in range(27, 53)]
+    red_list = [f"0" for i in range(1, 27)]
+    black_list = [f"1" for i in range(27, 53)]
     deck = red_list + black_list
 
     return deck
 
-# shuffle
-shuffled_decks = []
-for iter in range(1, 3):
-    deck = generate_deck()
-    random.shuffle(deck)
-    condensed_deck = "".join(deck)
+def shuffle_deck(rounds):
+    shuffled_decks = []
+    for iter in range(0, rounds):
+        deck = generate_deck()
+        random.shuffle(deck)
+        condensed_deck = "".join(deck)
 
-    shuffled_decks.append(condensed_deck)
-    
-#print(shuffled_decks[0][0:3])
+        shuffled_decks.append(condensed_deck)
+    return shuffled_decks
 
-print(shuffled_decks)
-print(type(shuffled_decks[0]))
-deck_1 = shuffled_decks[0]
-print(deck_1.count('R'))
-# store the raw deck data
-# try to not use .csv
+# save the shuffled decks to a file in the data subfolder
+def save_decks_to_file(shuffled_decks, filename):
+    with open(filename, "w") as f:
+        for deck in shuffled_decks:
+            f.write(deck + "\n")
+
+shuffle_decks = shuffle_deck(10)
+save_decks_to_file(shuffle_decks, "data/shuffled_decks10.txt")
