ad0262e49c5edfadf81d7f1feb2e736b09b0c825
Ilias Papageorgiou <thg9npb5ck@privaterelay.appleid.com>
2026-09-25 11:06:22 -0400

    Update dataproc.py

--- src/dataproc.py
+++ src/dataproc.py
@@ -78,3 +78,6 @@ def analyze_file(filename, num_decks):
     bits = unpack_bit_list(Path(filename).read_bytes(), num_decks)
     decks = convert_to_strings(bits, num_decks)
     return count_tricks(decks), len(decks)
+
+if __name__ == "__main__":
+    main()
